5580279773037c53c81e4ad8dabcd24b35c20c3c
Lucas da Matta <matat201@gmail.com>
2024-06-08 10:55:49 -0300

    adicionando telas de listagem de produtos

---
+++ app/src/main/java/br/gov/sp/fatec/eatsconsumer/descricao_produto.java
@@ -0,0 +1,14 @@
+package br.gov.sp.fatec.eatsconsumer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class descricao_produto extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_descricao_produto);
+    }
+}
